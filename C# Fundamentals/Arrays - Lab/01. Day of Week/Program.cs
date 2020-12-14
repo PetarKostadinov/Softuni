abc93b82652c927292351a74a6c8f36e183ee99f
Nikoleta Radoslavova <petarkostadinov30@gmail.com>
2020-12-14 11:40:16 +0200

    Done 100/100

---
+++ C# Fundamentals/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace orderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> myDic = new Dictionary<string, List<int>>();
+            List<string> people = Console.ReadLine().Split().ToList();
+
+            while (true)
+            {
+                if (people[0] == "End")
+                {
+                    break;
+                }
+
+                myDic.Add(people[0], new List<int>() { int.Parse(people[1]), int.Parse(people[2]) });
+
+                people = Console.ReadLine().Split().ToList();
+            }
+
+            foreach (var kvp in myDic.OrderBy(x => x.Value[1]))
+            {
+                Console.WriteLine($"{kvp.Key} with ID: {kvp.Value[0]} is {kvp.Value[1]} years old.");
+            }
+
+        }
+    }
+}
