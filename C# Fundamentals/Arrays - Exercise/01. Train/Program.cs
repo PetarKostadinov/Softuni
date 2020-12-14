714c928657ee1e173a9d35092b8ecdf13dbfdc92
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-14 12:25:44 +0200

    new comit

---
+++ C# Fundamentals/Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[] vowels = new char[] { 'E', 'e', 'U', 'u', 'I', 'i', 'O', 'o', 'A', 'a' };
+
+            int[] results = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                string name = Console.ReadLine();
+
+                for (int j = 0; j < name.Length; j++)
+                {
+                    char currIndex = name[j];
+
+                    if (vowels.Any(x => x == name[j]))
+                    {
+                        sum += currIndex * name.Length;
+                    }
+                    else
+                    {
+                        sum += currIndex / name.Length;
+
+                    }
+                }
+                results[i] = sum;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, results.OrderBy(x => x)));
+
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays - Lab/New folder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short firstEmp = short.Parse(Console.ReadLine());
+            short secondEmp = short.Parse(Console.ReadLine());
+            short thirdEmp = short.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            short time = 0;
+
+            int togetherForHour = firstEmp + secondEmp + thirdEmp;
+
+
+
+            while (peopleCount > 0)
+            {
+                time++;
+
+                if (time % 4 == 0)
+                {
+
+                    continue;
+
+                }
+
+                peopleCount -= togetherForHour;
+
+            }
+
+
+            Console.WriteLine($"Time needed: {time}h.");
+        }
+    }
+}
