9f281b393ab053e632a5bf7f924a5fc5d4f0c98f
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-24 21:26:55 +0200

    Done 100/100

---
+++ C# Fundamentals/List - More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = Console.ReadLine().Split().ToList();
+            string inputString = Console.ReadLine();
+
+            List<char> result = new List<char>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                string currentString = data[i];
+
+                int index = 0;
+                for (int j = 0; j < currentString.Length; j++)
+                {
+                    char currentElement = currentString[j];
+                    int currentElementValue = int.Parse(currentElement.ToString());
+
+                    index += currentElementValue;
+                }
+
+                index = index % inputString.Length;
+                result.Add(inputString[index]);
+
+                
+
+                string firstPart = inputString.Substring(0, index);
+                string secondPart = inputString.Substring(index + 1, inputString.Length - index - 1);
+
+                string temproraryString = firstPart + secondPart;
+                inputString = temproraryString;
+            }
+
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
---
+++ C# Fundamentals/List - More Exercise/02. Car Race/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] speedCounter = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            double firsrtCarr = 0;
+            double secondCarr = 0;
+            
+
+            int finishLine = (speedCounter.Length - 1) / 2;
+
+            for (int i = 0; i < finishLine; i++)
+            {
+                firsrtCarr += speedCounter[i];
+
+                if (speedCounter[i] <= 0)
+                {
+                    firsrtCarr *= 0.8;
+                }
+
+
+            }
+            for (int i = speedCounter.Length - 1; i >= finishLine + 1; i--)
+            {
+                secondCarr += speedCounter[i];
+
+                if (speedCounter[i] <= 0)
+                {
+                    secondCarr *= 0.8;
+                }
+            }
+
+            double winer = 0;
+            string firstOrSecond = string.Empty;
+
+            if (firsrtCarr > secondCarr)
+            {
+                 winer = secondCarr;
+
+                firstOrSecond = "right";
+            }
+            else
+            {
+                winer = firsrtCarr;
+
+                firstOrSecond = "left";
+            }
+
+            Console.WriteLine($"The winner is {firstOrSecond} with total time: {winer}");
+            
+        }
+    }
+}
