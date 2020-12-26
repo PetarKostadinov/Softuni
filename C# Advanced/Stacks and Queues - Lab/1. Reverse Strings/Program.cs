ceaec2884254f867df63e5382e869ab794a156f4
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-26 23:40:39 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+          
+        }
+    }
+}
