2d7333ca41b59f35ebd568364d7b552aa0245d22
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-27 12:36:05 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            while (true)
+            {
+                string[] comands = Console.ReadLine().ToLower().Split().ToArray();
+
+                if (comands[0] == "end")
+                {
+                    break;
+                }
+
+                else if (comands[0] == "add")
+                {
+                    stack.Push(int.Parse(comands[1]));
+                    stack.Push(int.Parse(comands[2]));
+                }
+                else if (comands[0] == "remove")
+                {
+                    int length = int.Parse(comands[1]);
+
+                    if (length <= stack.Count)
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                   
+                }
+
+            }
+            Console.WriteLine(stack.Sum());
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+
+            Stack<string> stack = new Stack<string>(input);
+
+
+            while (stack.Count > 1)
+            {
+
+                int first = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                switch (sign)
+                {
+                    case "+":stack.Push((first + second).ToString());  
+                    break;
+
+                    case "-": stack.Push((first - second).ToString());
+                        break;
+                }
+                
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
