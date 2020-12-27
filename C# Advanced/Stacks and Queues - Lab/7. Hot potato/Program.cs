1a4afd7d8ffe421cee405eff6934fb4e610201a9
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-27 22:57:26 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/7. Hot potato/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToArray();
+
+            Queue<string> queue = new Queue<string>(input);
+
+            int count = int.Parse(Console.ReadLine());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < count; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                Console.WriteLine($"Removed {queue.Peek()}");
+                queue.Dequeue();
+            }
+            Console.WriteLine($"Last is {queue.Peek()}");
+        }
+    }
+}
