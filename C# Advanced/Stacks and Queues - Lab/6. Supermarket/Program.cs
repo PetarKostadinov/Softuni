f9f1d03aca7149bf08cbb82414a57bc48197833f
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-27 20:02:50 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            Queue<string> queuePeople = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Paid")
+                {
+                    foreach (var item in queue)
+                    {
+                        queuePeople.Enqueue(item);
+                       
+                    }
+                    queue.Clear();
+                    continue;
+                }
+                if (input == "End")
+                {
+                    break;
+                }
+
+                queue.Enqueue(input);
+            }
+            foreach (var item in queuePeople)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
