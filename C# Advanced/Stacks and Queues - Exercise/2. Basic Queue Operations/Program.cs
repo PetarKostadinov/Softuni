9adf021976749952c56f96e46d7fda5abd4276e5
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-29 10:23:32 +0200

    Done 100/100

--- C# Advanced/Stacks and Queues - Exercise/2. Basic Queue Operations/Program.cs
+++ C# Advanced/Stacks and Queues - Exercise/2. Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Basic_Queue_Operations
 {
@@ -6,7 +8,42 @@ namespace Basic_Queue_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int nToPush = nsx[0];
+            int sToPop = nsx[1];
+            int xToLook = nsx[2];
+
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> stack = new Queue<int>();
+
+            for (int i = 0; i < nToPush; i++)
+            {
+                stack.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < sToPop; i++)
+            {
+                stack.Dequeue();
+            }
+
+
+            if (stack.Contains(xToLook))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count > 0 && !stack.Contains(xToLook))
+            {
+
+
+                Console.WriteLine(stack.Min());
+
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+
         }
     }
 }
