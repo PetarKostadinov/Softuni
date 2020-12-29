59ed572a16e2a51f97e80893e420d3e287b86406
PetarKostadinov <petarkostadinov30@gmail.com>
2020-12-29 10:15:49 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Exercise/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
