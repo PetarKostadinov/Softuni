a52dccb62eeb372245d0c4eb92027bc7363313f9
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-02 20:43:14 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Exercise/5. Fashion Boutique/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(sequens);
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            int shelf = 0;
+
+            int sum = 0;
+
+            while (stack.Count > 0)
+            {
+                var curent = stack.Peek();
+
+                if (sum + curent > capacity)
+                {
+                    shelf++;
+                    sum = 0;
+                }
+                else if (sum + curent == capacity)
+                {
+                    shelf++;
+                    sum = 0;
+                    stack.Pop();
+                }
+                else
+                {
+                    sum += stack.Pop();
+                }
+            }
+            if (sum > 0)
+            {
+                shelf++;
+            }
+
+            Console.WriteLine(shelf);  
+        }
+    }
+}
