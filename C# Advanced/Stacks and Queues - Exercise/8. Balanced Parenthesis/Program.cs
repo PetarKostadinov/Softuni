4b7c3f5a4e2a94242ce601a9c63508c8ad0ec6c5
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-03 20:43:00 +0200

    Done 100/100

---
+++ C# Advanced/Stacks and Queues - Exercise/8. Balanced Parenthesis/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _07.Balanced_Parentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var parantheses = Console.ReadLine()
+                .Trim()
+                .ToCharArray();
+
+            var stack = new Stack<char>();
+            var parenthesesPair = new Dictionary<char, char>();
+            parenthesesPair['{'] = '}';
+            parenthesesPair['['] = ']';
+            parenthesesPair['('] = ')';
+
+            if (parantheses.Length % 2 != 0 || parantheses.Length == 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            foreach (var p in parantheses)
+            {
+                //open parantheses
+                if (parenthesesPair.ContainsKey(p))
+                {
+                    stack.Push(p);
+                }
+                else
+                {
+                    var openParantheses = stack.Pop();
+                    if (parenthesesPair[openParantheses] != p)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("YES");
+        }
+    }
+}
