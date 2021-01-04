81d9f6f189c8ff8250badfdaf9f073dcd4f830f5
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-04 23:13:00 +0200

    Done 100/100

---
+++ C# Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            FillUpMatrix(matrix);
+
+            int count = 0;
+
+            for (int singleRow = 0; singleRow < rows - 1; singleRow++)
+            {
+                for (int singleCol = 0; singleCol < cols - 1; singleCol++)
+                {
+                    char currElement = matrix[singleRow, singleCol];
+
+                    if (currElement == matrix[singleRow, singleCol + 1] 
+                        && currElement == matrix[singleRow + 1, singleCol] 
+                        &&currElement == matrix[singleRow + 1, singleCol + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        public static void FillUpMatrix(char[,] num)
+        {
+            for (int row = 0; row < num.GetLength(0); row++)
+            {
+                char[] currRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < num.GetLength(1); col++)
+                {
+                    num[row, col] = currRow[col];
+                }
+            }
+        }
+    }
+}
