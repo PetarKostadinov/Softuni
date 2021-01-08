59d4290fa8f57134426427d110b1929bb950c543
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-08 22:42:13 +0200

    Done 100/100

---
+++ C# Advanced/Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var jagedArr = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jagedArr[i] = numbers;
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+                var comands = input.Split(' ');
+
+                string currCommand = comands[0];
+                int row = int.Parse(comands[1]);
+                int col = int.Parse(comands[2]);
+                int value = int.Parse(comands[3]);
+
+                if (row < 0 
+                    || row > jagedArr.Length - 1 
+                    || col < 0 
+                    || col > jagedArr[row].Length - 1)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (currCommand == "Add")
+                {
+                    jagedArr[row][col] += value;
+                }
+                else if (currCommand == "Subtract")
+                {
+                    jagedArr[row][col] -= value;
+                }
+            }
+
+            foreach (var array in jagedArr)
+            {
+                Console.WriteLine(string.Join(" ", array));
+
+            }
+        }
+    }
+}
