c63da87306bc32fc2956d70e5d16236a13002532
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-11 20:34:47 +0200

    Done 100/100

---
+++ C# Advanced/Defining Classes - Exercise/04. Opinion Poll/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+
+        public int Age { get => age; set => age = value; }
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+            : base()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> collection = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person human = new Person(name, age);
+
+                collection.Add(human);
+                
+            }
+
+            foreach (var item in collection.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+           
+        }
+
+    }
+}
