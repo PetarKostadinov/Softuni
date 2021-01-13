1f7ce77e603c0a8c9d2d19f5f9042e38978c76ca
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-13 21:34:15 +0200

    Done 100/100

---
+++ C# Advanced/Defining Classes - Lab/2. Car Extension/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        //private string make;
+        //private string model;
+        //private int year;
+
+        public string Make { get; set; }
+      
+        public string Model { get; set; }
+      
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }    //•	fuelQuantity: double  •	fuelConsumption: double
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * FuelConsumption;
+
+            if (this.FuelQuantity - consumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+
+        }
+        public string WhoAmI()
+        {
+           return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+
+         
+    }
+}
