8d62b97a02b77f54788664495ac6f5a2ef26a580
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-14 01:08:15 +0200

    Done 100/ 100

---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        //private string make;
+        //private string model;
+        //private int year;
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+           
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            :this (make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public string Make { get; set; }
+      
+        public string Model { get; set; }
+      
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }   
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * FuelConsumption;
+
+            if (this.FuelQuantity - consumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+
+
+    }
+}
