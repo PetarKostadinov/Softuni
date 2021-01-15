648876c68827c6a1162d7e1d333cec0cb3e89cca
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-15 10:26:13 +0200

    Done 100/100

---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public int GetTimeDiference(string startingDate, string endDate)
+        {
+            DateTime firstDate = DateTime.Parse(startingDate);
+            DateTime secondDate = DateTime.Parse(endDate);
+
+            int diffrence = (int)Math.Abs((firstDate - secondDate).TotalDays);
+
+            return diffrence;
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            var result = dateModifier.GetTimeDiference(startDate, endDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}
