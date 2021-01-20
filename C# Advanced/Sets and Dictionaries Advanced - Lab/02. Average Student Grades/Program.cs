4e3ca826d811b0a51b0a29a9ef3dabba9832b8cc
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-20 22:57:43 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (!students.ContainsKey(input[0]))
+                {
+                    students.Add(input[0], new List<decimal>());
+                    students[input[0]].Add(decimal.Parse(input[1]));
+                }
+                else
+                {
+                    students[input[0]].Add(decimal.Parse(input[1]));
+                }
+            }
+            foreach (var grades in students)
+            {
+                Console.Write($"{grades.Key} -> ");
+                foreach (var number in grades.Value)
+                {
+                    Console.Write($"{ number:F2} ");
+
+                }
+
+                Console.Write($"(avg: { grades.Value.Average():F2})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
