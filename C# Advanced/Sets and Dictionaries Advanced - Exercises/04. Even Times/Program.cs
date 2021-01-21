0873d33bce8be9c192948b3cf87339d6fe4dc59e
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 23:03:14 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercises/04. Even Times/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> collection = new Dictionary<int, int>();
+
+            int counter = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!collection.ContainsKey(num))
+                {
+                    counter = 1;
+
+                    collection.Add(num, counter);
+                }
+                else
+                {
+                    collection[num] += 1;
+                }
+            }
+
+            foreach (var num in collection.Where(x => x.Value % 2 == 0))
+            {
+                Console.WriteLine(num.Key);
+            }
+        }
+    }
+}
