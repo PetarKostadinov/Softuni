782874ed0eb96b948ff016755a3009c46e2d7754
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 10:50:45 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/06. Parking Lot - Sets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> nums = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0].Equals("END"))
+                {
+                    break;
+                }
+
+                string command = input[0];
+                string num = input[1];
+
+                if (command == "IN")
+                {
+                    nums.Add(num);
+                }
+                if (command == "OUT")
+                {
+                    if (nums.Contains(num))
+                    {
+                        nums.Remove(num);
+                    }
+                    
+                }
+            }
+            if (nums.Count != 0)
+            {
+                foreach (var num in nums)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
