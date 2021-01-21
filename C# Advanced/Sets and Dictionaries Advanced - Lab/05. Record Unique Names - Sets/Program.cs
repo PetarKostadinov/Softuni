d54cdd57a5b37702e1c93113de814d67c2773e1e
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 10:29:42 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/05. Record Unique Names - Sets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var input = Console.ReadLine();
+
+                names.Add(input);
+            }
+
+            foreach (var name  in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
