44937bf87761288b2ef60fae4603221168a32682
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 19:53:06 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercises/02. Sets of Elements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsofElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int first = nums[0];
+            int second = nums[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < first; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < second; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                secondSet.Add(num);
+            }
+
+            foreach (var num1 in firstSet)
+            {
+                foreach (var num2 in secondSet)
+                {
+                    if (num1 == num2)
+                    {
+                        Console.Write(num1 + " ");
+                    }
+                  
+                }
+            }
+        }
+    }
+}
