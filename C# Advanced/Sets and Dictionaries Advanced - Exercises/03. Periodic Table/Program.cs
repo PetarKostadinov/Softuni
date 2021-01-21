729472ebf45242719c5f25fd898554ebcdf466d4
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 20:54:59 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercises/03. Periodic Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            SortedSet<string> collection = new SortedSet<string>();
+
+            for (int i = 0; i < N ; i++)
+            {
+                var elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in elements)
+                {
+                    collection.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", collection));
+          
+        }
+    }
+}
