261bb40a657f75d22f483be6bdb57e0d43a6a53b
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 11:37:06 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/07. SoftUni Party - Sets/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "PARTY")
+                {
+                    break;
+                }
+
+                guests.Add(input);
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                guests.Remove(input);
+            }
+            Console.WriteLine(guests.Count);
+
+            HashSet<string> vipSetPrintedAndToBeRemoved = new HashSet<string>();
+
+            foreach (var guest in guests)
+            {    
+                var vipGuest = guest.ToCharArray();
+
+                if (Char.IsDigit(vipGuest[0]))
+                {
+                    Console.WriteLine(guest);
+                    vipSetPrintedAndToBeRemoved.Add(guest);
+                }
+            }
+            foreach (var guest in vipSetPrintedAndToBeRemoved)
+            {
+                guests.Remove(guest);
+
+            }
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
