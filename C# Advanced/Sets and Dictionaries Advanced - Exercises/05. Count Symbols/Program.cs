54e997afdcb4af3ef4c0791c410f765c8353e12e
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-21 23:32:44 +0200

    Done 100/100

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercises/05. Count Symbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            SortedDictionary<char, int> collection = new SortedDictionary<char, int>();
+
+            int counter = 0;
+
+            foreach (var item in input)
+            {
+                if (!collection.ContainsKey(item))
+                {
+                    counter = 1;
+                    collection.Add(item, counter);
+                }
+                else
+                {
+                    collection[item] += 1;
+                }
+            }
+            foreach (var item in collection)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
