9c0afd52faba56f73ab7fc7376cfffc339c43d31
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-22 20:07:16 +0200

    Done 100/100

---
+++ C# Advanced/Streams, Files and Directories - Lab/File Operations/Line Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../OutputText.txt"))
+                {
+
+                    string line = reader.ReadLine();
+                    int counter = 1;
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        Console.WriteLine($"{counter}. {line}");
+
+                        counter++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+
+              
+            }
+        }
+    }
+}
