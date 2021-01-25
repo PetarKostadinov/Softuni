69d98eaa3d9db6f995780ece49800476b6d4f105
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-25 21:21:12 +0200

    Done 100/100

---
+++ C# Advanced/Streams, Files and Directories - Exercise/Copy Binary File/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            using (FileStream reader = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (FileStream writer = new FileStream("../../../newFile.png", FileMode.Create))
+                {
+                    byte[] bufer = new byte[4096];
+
+                    while (reader.CanRead)
+                    {
+                        int byteRead = reader.Read(bufer, 0, bufer.Length);
+
+                        if (byteRead == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(bufer, 0, bufer.Length);
+                    }
+                }
+            }
+            
+        }
+    }
+}
