f4d510765c3c7a241c1eaf6e704446455e1c4a6a
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-25 22:15:31 +0200

    Done 100/100

---
+++ C# Advanced/Streams, Files and Directories - Exercise/Directory Traversal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> fileInfo = new Dictionary<string, Dictionary<string, double>>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../../");
+
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (!fileInfo.ContainsKey(file.Extension))
+                {
+                    fileInfo.Add(file.Extension, new Dictionary<string, double>());
+
+                    
+                }
+
+                fileInfo[file.Extension].Add(file.Name, file.Length / 1000.00);
+            }
+
+            using (StreamWriter writer = new StreamWriter($@"{ Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\DirectoriTraversal.txt"))
+            {
+
+                foreach (var item in fileInfo.OrderByDescending(x => x.Value.Count).ThenBy(i => i.Key))
+                {
+                    writer.WriteLine(item.Key);
+
+                    foreach (var file in item.Value.OrderByDescending(y => y.Value))
+                    {
+                        writer.WriteLine($"--{file.Key} - {file.Value}kb");
+                    }
+                }
+            }
+        }
+    }
+}
