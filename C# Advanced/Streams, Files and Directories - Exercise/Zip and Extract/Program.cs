69ebd456d89fd126e62a717cde0e121c7c21ce6d
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-25 23:20:58 +0200

    Done 100/100

---
+++ C# Advanced/Streams, Files and Directories - Exercise/Zip and Extract/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace ZipExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"C:\Users\niki_\Desktop\zipTake", @"C:\Users\niki_\Desktop\zipFileCreate\myZip.zip");
+
+            ZipFile.ExtractToDirectory(@"C:\Users\niki_\Desktop\zipFileCreate\myZip.zip", @"C:\Users\niki_\Desktop\zipResult\Rezult.zip");
+        }
+    }
+}
