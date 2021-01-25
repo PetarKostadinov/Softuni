eb5d01ca98df55cf23a6f05f50f659f2424e8fcd
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-25 20:17:38 +0200

    Done 100/100

---
+++ C# Advanced/Streams, Files and Directories - Exercise/Even Lines/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {  
+            using StreamReader reader = new StreamReader("../../../Text.txt");
+            {
+                string line = reader.ReadLine();
+                int lineCounter = 0;
+
+                while (line != null)
+                {
+                    //line.Replace('-', '@');
+                    //line.Replace(',', '@');
+                    //line.Replace('.', '@');
+                    //line.Replace('!', '@');
+                    //line.Replace('?', '@');
+                    //line.Replace('-', '@');
+                    // or Regex =>
+
+                    if (lineCounter % 2 == 0)
+                    {
+                        Regex patern = new Regex("[-,.!?]");
+
+                        line = patern.Replace(line, "@");
+
+                        var array = line.Split().ToArray().Reverse();
+
+                        Console.WriteLine(string.Join(" ", array));
+                    }
+
+                    lineCounter++;
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
