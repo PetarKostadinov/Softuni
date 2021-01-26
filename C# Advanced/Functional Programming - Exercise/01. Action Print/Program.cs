27228e1b254d1d893f232cdef0b5bd00e1b83086
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-26 15:38:54 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
