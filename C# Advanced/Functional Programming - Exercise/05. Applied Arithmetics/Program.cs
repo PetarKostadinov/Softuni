b8c06c0282d4af0795e5d00e8383150a0fd896d1
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-26 20:09:35 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int> subtractFunc = num => num -= 1;
+            Func<int, int> multiplyFunc = num => num *= 2;
+            Func<int, int> addFunc = num => num += 1;
+
+            Action<int[]> print = nums => Console.WriteLine(string.Join(" ", nums));
+
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            string comand = Console.ReadLine();
+
+            while (comand != "end")
+            {
+                if (comand == "add")
+                {
+                    numbers = numbers.Select(addFunc).ToArray();
+                }
+                else if (comand == "multiply")
+                {
+                    numbers = numbers.Select(multiplyFunc).ToArray();
+                }
+                else if (comand == "subtract")
+                {
+                    numbers = numbers.Select(subtractFunc).ToArray();
+                }
+                else if (comand == "print")
+                {
+                    print(numbers);
+                }
+
+                comand = Console.ReadLine();
+            }
+        }
+    }
+}
