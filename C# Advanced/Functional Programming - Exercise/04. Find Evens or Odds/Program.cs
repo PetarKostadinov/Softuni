8702b7e720605d8b7ee1688dabc65258e095464c
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-26 18:35:19 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEvenPredicate = num => num % 2 == 0;
+
+            Func<int, bool> isEvenFunc = myNum => myNum % 2 == 0;
+
+            Action<List<int>> printNUmbers = nums => Console.WriteLine(string.Join(" ", nums));
+
+            int[] input = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+
+            List<int> numbers = new List<int>();
+
+            int startNum = input[0];
+            int endNum = input[1];
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                numbers.Add(i);
+            }
+
+            List<int> result = new List<int>();
+
+            string numbersTipe = Console.ReadLine();
+
+            if (numbersTipe == "even")
+            {
+                result = numbers.Where(isEvenFunc).ToList();
+            }
+            else
+            {
+               result = numbers.FindAll(x => !isEvenPredicate(x));
+            }
+            printNUmbers(result);
+        }
+    }
+}
