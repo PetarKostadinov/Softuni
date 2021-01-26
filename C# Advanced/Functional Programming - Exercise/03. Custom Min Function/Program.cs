fbbd53260e8e4ade32d6a8f34eccdd4a84b72088
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-26 17:27:56 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int[], int> minFunc = inputNumbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var currNum in inputNumbers)
+                {
+                    if (currNum < minValue)
+                    {
+                        minValue = currNum;
+                    }
+                }
+
+                return minValue;
+            };
+
+            int[] numbers = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToArray();
+
+            int result = minFunc(numbers);
+
+            Console.WriteLine(result);
+        }
+    }
+}
