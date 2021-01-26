af081e6dc7b806f235fe56b1544c5f358615ea84
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-26 22:54:19 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/08. Custom Comparator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> myCustomComparer = (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 == 0)
+                {
+                    if (a < b)
+                    {
+                        return -1;
+                    }
+
+                    if (a > b)
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                }
+
+                if (a % 2 != 0 && b % 2 != 0)
+                {
+                    if (a < b)
+                    {
+                        return -1;
+                    }
+
+                    if (a > b)
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                }
+
+                if (a % 2 == 0)
+                {
+                    return -1;
+                }
+
+                if (a % 2 != 0)
+                {
+                    return 1;
+                }
+
+                return 0;
+            };
+
+            Array.Sort(input, new Comparison<int>(myCustomComparer));
+
+            Console.WriteLine(string.Join(" ", input));
+
+        }
+        public static int CompareStrings(string s1, string s2)
+        {
+            return 0;
+        }
+
+    }
+}
+
