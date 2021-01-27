abaca1a42a256ada965022370b0984df521fb246
PetarKostadinov <petarkostadinov30@gmail.com>
2021-01-27 15:59:27 +0200

    Done 100/100

---
+++ C# Advanced/Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<string, int, bool> isEqualOrLargerFunc = (word, criteria) => word
+            .Sum(x => x) >= criteria;
+
+            int targetSum = int.Parse(Console.ReadLine());
+            string[] inputNames = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            Func<string[], Func<string, int, bool>, string> myFunc = (names, isLargerFunc) => names
+            .FirstOrDefault(x => isEqualOrLargerFunc(x, targetSum));
+
+            string targetName = myFunc(inputNames, isEqualOrLargerFunc);
+
+            Console.WriteLine(targetName);
+        }
+    }
+}
