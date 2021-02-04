b14f90e4f5155edb1957607ef8bd932b01853447
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-04 13:50:08 +0200

    Done 100/100

---
+++ C# Advanced/C# Advanced Retake Exam - 16 December 2020/Opening/Bakery.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        public void Add(Employee employee)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(employee);
+                //Capacity--;
+            }
+        }
+        public bool Remove(string name)
+        {
+            Employee employeeToRemove = data.FirstOrDefault(x => x.Name == name);
+
+            if (employeeToRemove != null)
+            {
+                data.Remove(employeeToRemove);
+
+                return true;
+            }
+            return false;
+        }
+
+
+        public Employee GetOldestEmployee()
+        {
+            var oldest = data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldest;
+        }
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = data.FirstOrDefault(x => x.Name == name);
+
+            return employee;
+        }
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var person in data)
+            {
+                result.AppendLine(person.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced Retake Exam - 16 December 2020/Opening/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public override string ToString()
+        {
+            var result = $"Eployee {this.Name}, {this.Age} ({this.Country})";
+
+            return result;
+
+
+        }
+    }
+}
