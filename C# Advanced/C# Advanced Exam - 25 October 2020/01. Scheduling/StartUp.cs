43fb8efc1fcb778dae2b30ac1ec63f32561c4a41
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-06 13:41:56 +0200

    Done 100/100

---
+++ C# Advanced/C# Advanced Exam - 25 October 2020/01. Scheduling/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] givenTasks = Console.ReadLine()
+                              .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                              .Select(int.Parse)
+                              .ToArray();
+                              
+            Stack<int> tasks = new Stack<int>(givenTasks);
+
+            int[] givenTreads = Console.ReadLine()
+                               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+
+            Queue<int> threads = new Queue<int>(givenTreads);
+
+            int target = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (threads.Peek() >= tasks.Peek())
+                {
+                    if (tasks.Peek() == target)
+                    {
+                        break;
+                    }
+
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else if (threads.Peek() < tasks.Peek())
+                {
+                    if (tasks.Peek() == target)
+                    {
+                        break;
+                    }
+
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {target}");
+
+            Console.WriteLine(string.Join(" ", threads));
+        }
+    }
+}
