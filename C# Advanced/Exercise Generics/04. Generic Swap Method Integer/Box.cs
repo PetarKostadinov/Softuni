a40651dca51f579b063169297946f4e96d2b3c08
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-08 22:29:40 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+   public class Box<T>
+    {
+        public Box()
+        {
+            this.Value = new List<T>();
+        }
+
+        public List<T> Value { get; set; }
+        public void Swap(int a, int b)
+        {
+            T tempValue = this.Value[a];
+            this.Value[a] = this.Value[b];
+            this.Value[b] = tempValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var item in this.Value)
+            {
+                result.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return result.ToString().TrimEnd() ;
+        }
+    }
+}
