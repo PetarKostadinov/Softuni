1e06e30e75ac44ef0cc58fd488b46884139e852f
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-08 22:25:03 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {     
+            int n = int.Parse(Console.ReadLine());
+
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Value.Add(input);
+
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int a = indexes[0];
+            int b = indexes[1];
+
+            box.Swap(a, b);
+            Console.WriteLine(box);
+        }
+    }
+}
