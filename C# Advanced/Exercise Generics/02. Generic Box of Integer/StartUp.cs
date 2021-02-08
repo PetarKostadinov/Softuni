c644452038e30fe3189bcc557a1a605239d3d7ff
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-08 21:31:13 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generics
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {     
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int text = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(text);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
