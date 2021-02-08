427756ab1cb8fc57c2d3503fae0213e7c649d38c
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-08 20:57:32 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/01. Generic Box of String/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+   public class Box<T>
+    {
+       
+
+        public Box(T value)
+        {
+            this.Value = value;
+          
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"System.String: {this.Value}";
+        }
+    }
+}
---
+++ C# Advanced/Exercise Generics/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+
+                Box<string> box = new Box<string>(text);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
