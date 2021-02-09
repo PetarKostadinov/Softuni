a086a02fabdcb30563e19f5a1720072691ae4883
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-09 23:44:10 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/07. Tuple/StartUp.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Generics
+{
+    using System;
+    using System.Linq;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPersonInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] inputBeer = Console.ReadLine().Split();
+
+            string[] inputNumbersInfo = Console.ReadLine().Split();
+
+            string fulName = inputPersonInfo[0] + ' ' + inputPersonInfo[1];
+            string adres = inputPersonInfo[2];
+
+            string name = inputBeer[0];
+            int liters = int.Parse(inputBeer[1]);
+
+            int myInt = int.Parse(inputNumbersInfo[0]);
+            double myDouble = double.Parse(inputNumbersInfo[1]);
+
+            var personInfo = new MyTuple<string, string>(fulName, adres);
+            var beer = new MyTuple<string, int>(name, liters);
+            var numbersInfo = new MyTuple<int, double>(myInt, myDouble);
+
+            Console.WriteLine(personInfo);
+            Console.WriteLine(beer);
+            Console.WriteLine(numbersInfo);
+        }
+    }
+}
