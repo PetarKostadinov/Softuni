85d96b651d0203378a33f5cd362c2257c293d35f
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-09 21:28:02 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                box.Value.Add(input);
+            }
+            double targetItem = double.Parse(Console.ReadLine());
+            int result = box.isGreaterThan(targetItem);
+            Console.WriteLine(result);
+        }
+    }
+}
