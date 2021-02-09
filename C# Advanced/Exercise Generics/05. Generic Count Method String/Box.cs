85d171b6e911272c0157fa15e91fc787eaa5ba75
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-09 21:14:30 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/05. Generic Count Method String/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+   public class Box<T>
+         where T : IComparable
+    {
+        public Box()
+        {
+            this.Value = new List<T>();
+        }
+
+        public List<T> Value { get; set; }
+
+        public int isGreaterThan(T targetItem)
+        {
+            int counter = 0;
+
+            foreach (var value in this.Value)
+            {
+                if (value.CompareTo(targetItem) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public void Swap(int a, int b)
+        {
+            T tempValue = this.Value[a];
+            this.Value[a] = this.Value[b];
+            this.Value[b] = tempValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var item in this.Value)
+            {
+                result.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return result.ToString().TrimEnd() ;
+        }
+    }
+}
---
+++ C# Advanced/Exercise Generics/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Value.Add(input);
+            }
+            string targetItem = Console.ReadLine();
+            int result = box.isGreaterThan(targetItem);
+            Console.WriteLine(result);
+        }
+    }
+}
