026b02fc030b292b7f0a0761c4f61194036e95b4
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-10 10:49:26 +0200

    Done 100/100

---
+++ C# Advanced/Exercise Generics/08. Threeuple/StartUp.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Generics
+{
+    using System;
+    using System.Linq;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPersonInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] inputBeer = Console.ReadLine().Split();
+
+            string[] inputBankInfo = Console.ReadLine().Split();
+
+            string fulName = inputPersonInfo[0] + ' ' + inputPersonInfo[1];
+            string adres = inputPersonInfo[2];
+            string town = inputPersonInfo[3];
+
+
+            string name = inputBeer[0];
+            int liters = int.Parse(inputBeer[1]);
+            string type = inputBeer[2];
+
+            bool isDrunk = type == "drunk" ? true : false;
+           
+            string personName = inputBankInfo[0];
+            double balance = double.Parse(inputBankInfo[1]);
+            string bankName = inputBankInfo[2];
+
+            var personInfo = new Threeuple<string, string, string>(fulName, adres, town);
+
+            var beerInfo = new Threeuple<string, int, bool>(name, liters, isDrunk);
+
+            var bankInfo = new Threeuple<string, double, string>(personName, balance, bankName);
+
+            Console.WriteLine(personInfo);
+            Console.WriteLine(beerInfo);
+            Console.WriteLine(bankInfo);
+
+        }
+
+          
+    }
+}
---
+++ C# Advanced/Exercise Generics/08. Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Generics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Threeuple<TItem1, TItem2, TItem3>
+    {
+        public Threeuple(TItem1 item1, TItem2 item2, TItem3 item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public TItem1 Item1 { get; set; }
+        public TItem2 Item2 { get; set; }
+        public TItem3 Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
