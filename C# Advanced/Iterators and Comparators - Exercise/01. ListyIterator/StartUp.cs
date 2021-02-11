737cff0f7f9794bc97b146dd1974f08a5ea343eb
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-11 20:26:23 +0200

    Done 100/100

---
+++ C# Advanced/Iterators and Comparators - Exercise/01. ListyIterator/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<string> elements = command.Split().Skip(1).ToList();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(elements);
+
+            while (command != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message); 
+                    }
+                }
+                else if (command == "Move")
+                {
+                   bool result = listyIterator.Move();
+                    Console.WriteLine(result);
+                }
+                else if (command == "HasNext")
+                {
+                   bool result = listyIterator.HasNext();
+                    Console.WriteLine(result);
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
