1b73ace1337e396c33f87b7cd2a05ac498945466
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-12 00:00:51 +0200

    Done 100/100

---
+++ C# Advanced/Iterators and Comparators - Exercise/03. Stack/MyStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public MyStack()
+        {
+            items = new List<T>();
+        }
+
+        public int Count => this.items.Count;
+
+        public void Push(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = this.items[^1];
+
+            this.items.Remove(item);
+
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+
+            string commandInput = Console.ReadLine();
+
+            while (commandInput != "END")
+            {
+                string[] tokens = commandInput.Split(new string[] { " ", ","}, StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command == "Push")
+                {
+                    foreach (var item in tokens.Skip(1))
+                    {
+                        stack.Push(int.Parse(item));
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                    else
+                    {
+                       stack.Pop();
+                    }
+                }
+
+                commandInput = Console.ReadLine();
+            }
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
