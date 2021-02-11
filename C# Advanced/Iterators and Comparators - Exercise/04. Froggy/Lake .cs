d0aa09ae6f336c286df77cac0df2bcf43a964505
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-11 21:27:31 +0200

    Done 100/100

---
+++ C# Advanced/Iterators and Comparators - Exercise/04. Froggy/Lake .cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    yield return stones[i];
+                }
+                
+            }
+
+            for (int i = this.stones.Count - 1; i >= 0; i--)
+            {
+
+                if (i % 2 != 0)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> stones = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            Lake lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
