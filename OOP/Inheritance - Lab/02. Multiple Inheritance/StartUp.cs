0eac30e443d01c700c1d5542b605293f5e9b733f
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-22 14:59:14 +0200

    Done 100/100

---
+++ OOP/Inheritance - Lab/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Farm
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+
+            Puppy puppy = new Puppy();
+
+            puppy.Bark();
+            puppy.Eat();
+            puppy.Weep();
+
+        }
+    }
+}
