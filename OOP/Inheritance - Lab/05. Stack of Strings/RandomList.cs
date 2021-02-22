2a73af3f8198b6df80b9fd62cbd8849e8b279fe2
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-22 20:13:31 +0200

    Done 100/100

---
+++ OOP/Inheritance - Lab/05. Stack of Strings/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            bool isTrue = this.Count == 0 ? true : false;
+
+            return isTrue;
+        }
+
+        public void AddRange(Stack<string> range)
+        {
+            while (range.Count > 0)
+            {
+                this.Push(range.Pop());
+            }
+            
+        }
+    }
+}
