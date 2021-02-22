4a2bcd4f30c6a7a9e7981686874a94c37341a0a7
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-22 19:43:12 +0200

    Done 100/100

---
+++ OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public RandomList()
+        {
+            this.random = random;
+        }
+
+        public  Random random { get; set; }
+
+        public string RandomString()
+        {
+            int index = random.Next(0, this.Count);
+
+            string itemToRemove = this[index];
+
+            this.RemoveAt(index);
+
+            return itemToRemove;
+        }
+    }
+}
---
+++ OOP/Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            RandomList list = new RandomList();
+            list.Add("Pesho");
+            list.Add("Misho");
+            list.Add("Gosho");
+            list.Add("Mincho");
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+        }
+    }
+}
