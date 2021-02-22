99e48b3c6fbf060c3a367c38727badf07465f4ad
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-22 15:04:44 +0200

    Done 100/100

---
+++ OOP/Inheritance - Lab/03. Hierarchical Inheritance/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Farm
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Eat();
+
+            Puppy puppy = new Puppy();
+
+            puppy.Bark();
+            puppy.Eat();
+            puppy.Weep();
+
+            Cat cat = new Cat();
+
+            cat.Eat();
+            cat.Meow();
+
+        }
+    }
+}
