320061bc4ab90a1cce3455e973dc81397191456e
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-22 22:17:33 +0200

    Done 100/100

---
+++ OOP/Exercise Inheritance/01. Person/Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) 
+            : base(name, age)
+        {
+            
+        }
+
+        public int ChildAge 
+        {
+            get
+            {
+                return this.Age;
+            }
+            set
+            {
+                if (value < 0 && value > 15)
+                {
+                    throw new Exception();
+                }
+
+                this.ChildAge = value;
+            }
+        }
+    }
+}
---
+++ OOP/Exercise Inheritance/01. Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+   public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string  Name { get; set; }
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(string.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+
+            return sb.ToString();
+        }
+    }
+}
