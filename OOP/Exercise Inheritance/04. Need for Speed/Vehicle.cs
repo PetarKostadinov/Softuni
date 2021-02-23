4cd30aa088d1f6066d6bbf2adf59c1664d24365a
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-23 21:34:53 +0200

    Done 100/100

---
+++ OOP/Exercise Inheritance/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+   public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+
+
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+
+
+        public virtual void Drive(double kilometers)
+        {
+            bool canDrive = this.Fuel - this.FuelConsumption * kilometers >= 0;
+
+            if (canDrive)
+            {
+                this.Fuel -= this.FuelConsumption * kilometers;
+            }
+
+        }
+    }
+}
