064623f79ee6a15ca3f7213c2242484287614658
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-23 22:58:15 +0200

    Done 100/100

---
+++ OOP/Exercise Inheritance/05. Restaurant/Tea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Tea : HotBeverage
+    {
+       
+
+        public Tea(string name, decimal price, double mll) 
+            : base(name, price, mll)
+        {
+            
+        }
+
+       
+    }
+}
