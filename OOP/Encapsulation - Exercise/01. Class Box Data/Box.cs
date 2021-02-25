dee2325b008b077bf700d4683dd892fa49007e73
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-25 16:03:13 +0200

    Done 100/100

---
+++ OOP/Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+   public class Box
+    {
+
+        private const double MinimumSizeObject = 0;
+        private const string InvalidSideMessage = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length  
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                ValidateSide(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+             }
+            private set
+            {
+                ValidateSide(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                ValidateSide(value, nameof(this.Height));
+
+                this.height = value;
+            }
+        }
+
+        public double CalculateSuRfaceArea()
+        {
+            double result = (2 * this.Length * this.Width) 
+                          + (2 * this.Length * this.Height) 
+                          + (2 * this.Width * this.Height);
+            return result;
+        }
+
+        public double CalculateLateralSurface()
+        {
+            double result = (2 * this.Length * this.Height) 
+                          + (2 * this.Width * this.Height );
+
+            return result;
+        }
+
+        public double CalculateVolume()
+        {
+            double result = this.Length * this.Width * this.Height;
+
+            return result;
+        }
+
+        private void ValidateSide(double value, string name)
+        {
+            if (value <= MinimumSizeObject)
+            {
+                throw new ArgumentException(string.Format(InvalidSideMessage, name));
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {CalculateSuRfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {CalculateLateralSurface():f2}");
+            sb.AppendLine($"Volume - {CalculateVolume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ OOP/Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(l, w, h);
+
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
