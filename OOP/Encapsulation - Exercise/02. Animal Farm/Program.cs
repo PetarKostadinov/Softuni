4ecffef679690c83a614057f3395ddb1b03e385d
PetarKostadinov <petarkostadinov30@gmail.com>
2021-02-25 17:51:54 +0200

    Done 100/100

---
+++ OOP/Encapsulation - Exercise/02. Animal Farm/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AnimalFarm.Models
+{
+    using System;
+    
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (System.Exception ex)
+            {
+
+                Console.WriteLine(ex.Message); ;
+            }
+        }
+    }
+}
