566af0635613259fefa6b4eb4b4782fd4b60e95f
PetarKostadinov <petarkostadinov30@gmail.com>
2021-03-04 23:30:57 +0200

    Done 100/100

---
+++ OOP/Interfaces and Abstraction - Exercise/05. Birthday Celebrations/Citizens.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizens : IIdentification, IType, IBirthdates
+    {
+        private string name;
+        private int age;
+        public Citizens(string type, string name, int age, string id, string birthdate)
+        {
+            this.Type = type;
+            this.name = name;
+            this.age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+        public string Id { get; private set; }
+
+        public string Type { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
---
+++ OOP/Interfaces and Abstraction - Exercise/05. Birthday Celebrations/StarUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IBirthdates> animals = new List<IBirthdates>(); 
+
+            while (true)
+            {
+                var data = Console.ReadLine().Split().ToList();
+
+                if (data[0] == "End")
+                {
+                    break;
+                }
+
+                if (data.Count == 5)
+                {
+                    string type = data[0];
+                    string name = data[1];
+                    int age = int.Parse(data[2]);
+                    string id = data[3];
+                    string birthdate = data[4];
+                   
+                    animals.Add(new Citizens(type, name, age, id, birthdate));
+                }
+                else if (data.Count == 3)
+                {
+                    string type = data[0];
+                    string model = data[1];
+                    string id = data[2];
+
+                    if (type == "Pet")
+                    {
+                        string name = model;
+                        string birthdate = id;
+                    
+                        animals.Add(new Pets(type, name, birthdate));
+                    }
+                }
+            }
+            string endingDigits = Console.ReadLine();
+
+            foreach (var member in animals.Where(x => x.Birthdate.EndsWith(endingDigits)))
+            {
+                Console.WriteLine(member.Birthdate);
+            }
+        }
+    }
+}
