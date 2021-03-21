f027f4263912aa848a81b9440bae532944038fe1
PetarKostadinov <petarkostadinov30@gmail.com>
2021-03-21 21:52:48 +0200

    InProgres

---
+++ OOP/C# OOP Retake Exam - 22 August 2020/02. Business Logic/Models/Cars/Entities/Car.cs
@@ -0,0 +1,72 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+
+        private string model;
+        private int horsePower;
+      
+        private int minHorsePower;
+        private int maxHorsePower;
+        private double cubicCentimeters;
+
+        protected Car(string model, int horsePower, double cubicCentimeters, int minHorsePower, int maxHorsePower)
+        {
+            this.minHorsePower = minHorsePower;
+            this.maxHorsePower = maxHorsePower;
+
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.CubicCentimeters = cubicCentimeters;
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            private set
+            {
+                if (value.Length < 4 || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Model {value} cannot be less than 4 symbols.");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            private set
+            {
+                if (value < this.minHorsePower || value > this.maxHorsePower)
+                {
+                    throw new ArgumentException($"Invalid horse power: {value}.");
+                }
+                this.horsePower = value;
+            }
+        }
+
+
+        public double CubicCentimeters { get; }
+
+
+        public double CalculateRacePoints(int laps)
+        {
+            double racePoints = this.CubicCentimeters / this.horsePower * laps;
+
+            return racePoints;
+        }
+    }
+}
---
+++ OOP/C# OOP Retake Exam - 22 August 2020/02. Business Logic/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using EasterRaces.Core.Contracts;
+using EasterRaces.IO;
+using EasterRaces.IO.Contracts;
+using EasterRaces.Core.Entities;
+using EasterRaces.Core;
+
+namespace EasterRaces
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            IChampionshipController controller = new ChampionshipController();
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            Engine enigne = new Engine(controller, reader, writer);
+            enigne.Run();
+        }
+    }
+}
