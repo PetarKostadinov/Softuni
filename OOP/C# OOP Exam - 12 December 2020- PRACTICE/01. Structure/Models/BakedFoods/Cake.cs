45a296ae5e54681c8db7be70bd389d4c5fd21286
PetarKostadinov <petarkostadinov30@gmail.com>
2021-03-25 12:24:54 +0200

    Done 50/50

---
+++ OOP/C# OOP Exam - 12 December 2020- PRACTICE/01. Structure/Models/BakedFoods/Cake.cs
@@ -0,0 +1,16 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods
+{
+    public class Cake : BakedFood
+    {
+        private const int InitialCakePortion = 245;
+        public Cake(string name, decimal price)
+            : base(name, InitialCakePortion, price)
+        {
+        }
+    }
+}
