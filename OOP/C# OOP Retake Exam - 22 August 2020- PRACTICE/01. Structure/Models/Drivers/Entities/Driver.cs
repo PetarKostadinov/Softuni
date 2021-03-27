c4378f1ef3229b831effb3cafcceedd40a79ece0
PetarKostadinov <petarkostadinov30@gmail.com>
2021-03-27 22:22:52 +0200

    Done 50/50

---
+++ OOP/C# OOP Retake Exam - 22 August 2020- PRACTICE/01. Structure/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,76 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Cars.Entities;
+using EasterRaces.Models.Drivers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public class Driver : IDriver
+    {
+        private string name;
+        private List<ICar> cars;
+        private bool canParticipate;
+
+        public Driver(string name)
+        {
+            this.Name = name;
+            this.cars = new List<ICar>();
+        }
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (value.Length < 5 && string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException($"Name {value} cannot be less than 5 symbols.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public ICar Car => (ICar)this.cars;
+
+        public int NumberOfWins { get; private set; }
+
+        public bool CanParticipate
+        {
+            get
+            {
+                return this.canParticipate;
+            }
+            private set
+            {
+                if (this.Car != null)
+                {
+                    value = true;
+                }
+
+                this.canParticipate = value;
+            }
+        }
+
+        public void AddCar(ICar car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentNullException("Car cannot be null.");
+            }
+
+            this.cars.Add(car);
+            this.CanParticipate = true;
+        }
+
+        public void WinRace()
+        {
+
+            this.NumberOfWins++;
+        }
+    }
+}
---
+++ OOP/C# OOP Retake Exam - 22 August 2020- PRACTICE/01. Structure/Repositories/Entities/DriverRepository.cs
@@ -0,0 +1,37 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class DriverRepository : IRepository<IDriver>
+    {
+        private readonly List<IDriver> drivers;
+        public DriverRepository()
+        {
+            this.drivers = new List<IDriver>();
+        }
+        public void Add(IDriver model)
+        {
+            this.drivers.Add(model);
+        }
+
+        public IReadOnlyCollection<IDriver> GetAll()
+        {
+            return this.drivers;
+        }
+
+        public IDriver GetByName(string name)
+        {
+            return this.drivers.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(IDriver model)
+        {
+            return this.drivers.Remove(model);
+        }
+    }
+}
