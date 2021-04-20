4f617f12d32e0473108b6923c7804a6d64f634d3
PetarKostadinov <petarkostadinov30@gmail.com>
2021-04-20 19:59:31 +0300

    IO- Correct- No Judge To Verify

---
+++ OOP/OOP - ExamPreparation/C# OOP Exam - 19 December 2019/02. Business Logic/Models/Dwarfs/Dwarf.cs
@@ -0,0 +1,70 @@
+﻿using SantaWorkshop.Models.Dwarfs.Contracts;
+using SantaWorkshop.Models.Instruments.Contracts;
+using SantaWorkshop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantaWorkshop.Models.Dwarfs
+{
+    public abstract class Dwarf : IDwarf
+    {
+        private string name;
+        private int energy;
+        private ICollection<IInstrument> instruments;
+
+        protected Dwarf(string name, int energy)
+        {
+            this.Name = name;
+           this.Energy = energy;
+            this.instruments = new List<IInstrument>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidDwarfName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Energy
+        {
+            get
+            {
+                return this.energy;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+
+                this.energy = value;
+            }
+        }
+
+        public ICollection<IInstrument> Instruments => this.instruments;
+
+        public void AddInstrument(IInstrument instrument)
+        {
+            
+            this.instruments.Add(instrument);
+        }
+
+        public virtual void Work()
+        {
+            this.Energy -= 10;
+        }
+    }
+}
---
+++ OOP/OOP - ExamPreparation/C# OOP Exam - 19 December 2019/02. Business Logic/Models/Dwarfs/HappyDwarf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantaWorkshop.Models.Dwarfs
+{
+    public class HappyDwarf : Dwarf
+    {
+        private const int InitialEnergy = 100;
+        public HappyDwarf(string name) 
+            : base(name, InitialEnergy)
+        {
+           
+        }
+
+        
+    }
+}
