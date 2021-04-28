e31ea37451cfa59f07639d74c045336cd70f2da6
PetarKostadinov <petarkostadinov30@gmail.com>
2021-04-28 23:08:22 +0300

    In Progress

---
+++ JavaScript/Arrays and Nested Arrays - Lab/02. Last K Numbers Sequence/exercise.js
@@ -0,0 +1,34 @@
+
+function solve(n, k) {
+
+    let array = [1, 1, 2];
+    const firstKvalue = k;
+
+    for (let i = 0; i < n - 3; i++) {
+
+        let sum = 0;
+
+        k = firstKvalue;
+
+
+        if (k > array.length) {
+
+            k = array.length;
+        }
+
+        for (let f = array.length-1; f > array.length - k - 1; f--) {
+
+            sum += array[f];
+
+        }
+
+        array[array.length] = Number(sum);
+    }
+
+
+    return array.join(', ');
+}
+console.log(solve(9, 5))
+console.log(solve(6, 3));
+console.log(solve(8, 2));
+
