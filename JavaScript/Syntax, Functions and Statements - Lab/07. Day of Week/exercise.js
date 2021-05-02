1176497ed020eca38f8994994d0b9aa1efec48ce
PetarKostadinov <petarkostadinov30@gmail.com>
2021-05-03 00:07:39 +0300

    Done 100/100

---
+++ JavaScript/Syntax, Functions and Statements - Lab/07. Day of Week/exercise.js
@@ -0,0 +1,41 @@
+function solve(operator) {
+
+    let result = '';
+
+    switch (operator) {
+        case 'Monday':
+            result = 1;
+            break;
+        case 'Tuesday':
+            result = 2;
+            break;
+        case 'Wednesday':
+            result = 3;
+            break;
+        case 'Thursday':
+            result = 4;
+            break;
+        case 'Friday':
+            result = 5;
+            break;
+        case 'Saturday':
+            result = 6;
+            break;
+        case 'Sunday':
+            result = 7;
+            break;
+
+        default:
+            result = 'error';
+
+    }
+
+    return result;
+}
+
+console.log(solve('Monday'))
+console.log(solve('Friday'))
+console.log(solve('Invalid'))
+
+
+
