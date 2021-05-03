b11e2a209a1776d2d5b1486452a87371ab5b7530
PetarKostadinov <petarkostadinov30@gmail.com>
2021-05-03 21:01:19 +0300

    Done 100/100

---
+++ JavaScript/Arrays and Nested Arrays - Exercise/02. Print every N-th Element from an Array/aternative solving/exercise.js
@@ -0,0 +1,24 @@
+function solve(array, step) {
+
+     return array.filter((_, index) => index % step === 0);
+
+    return result;   
+}
+
+console.log(solve(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2
+));
+console.log(solve(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+));
+
+
+
+
