de0c80faf388c75d2547bc9a7d2bc522ea5ba66a
PetarKostadinov <petarkostadinov30@gmail.com>
2021-05-03 20:31:11 +0300

    Done 100/100

---
+++ JavaScript/Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter/exercise.js
@@ -0,0 +1,24 @@
+function solve(array, delimiter) {
+
+    return array.join(delimiter);
+}
+
+console.log(solve(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+));
+console.log(solve(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+));
+
+
+
+
