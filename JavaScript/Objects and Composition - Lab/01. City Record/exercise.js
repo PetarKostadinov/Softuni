a8733a422ee005ca4b24fe80f549281aad8aab3b
PetarKostadinov <petarkostadinov30@gmail.com>
2021-05-04 12:50:58 +0300

    Done 100/100

---
+++ JavaScript/Objects and Composition - Lab/01. City Record/exercise.js
@@ -0,0 +1,17 @@
+
+function cityObj(name, population, treasury) {
+
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury
+
+    }
+
+    return city
+}
+
+console.log(cityObj('Tortuga',
+    7000,
+    15000
+))
