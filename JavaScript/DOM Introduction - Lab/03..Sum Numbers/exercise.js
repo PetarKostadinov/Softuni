e870d6dce4ea3e1ade6a35dd0308ffeb743a02c7
PetarKostadinov <petarkostadinov30@gmail.com>
2021-05-19 23:32:57 +0300

    Done 100/100

--- JavaScript/DOM Introduction - Lab/03..Sum Numbers/exercise.js
+++
@@ -1,5 +0,0 @@
-function calc() {
-    const x = Number(document.getElementById('num1').value);
-    const y = Number(document.getElementById('num2').value);
-    document.getElementById('sum').value = x + y;
-}
