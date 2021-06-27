9e57ff4310728a6c75dd5ad65497192331c0c8d9
PetarKostadinov <petarkostadinov30@gmail.com>
2021-06-27 17:32:17 +0300

    In Progress

--- JavaScript/DOM Introduction - Lab/02.Collect List Items/exercise.js
+++
@@ -1,5 +0,0 @@
-function extractText() {
-    const lielements = document.getElementsByTagName('li');
-    const content = [...lielements].map((el) => el.textContent);
-    document.getElementById('result').value = content.join('\n');
-}
