86dbcafa7d53d8ace170d8f8d1626cfd45b7563a
PetarKostadinov <petarkostadinov30@gmail.com>
2022-09-26 22:02:06 +0300

    100/100

--- JavaScript/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JavaScript/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+  let arrText = input.split('.');
+
+  for (let i = 0; i < arrText.length; i+= 3) {
+
+    let result = [];
+
+    for (let x = 0; x < 3; x++) {
+
+      if (arrText[i + x]){
+
+        result.push(arrText[i + x]);
+      };
+    };
+
+    let resultText = result.join('. ') + '.'.trim();
+
+    output.innerHTML += `<p>${resultText}</p>`
+  };
 }
