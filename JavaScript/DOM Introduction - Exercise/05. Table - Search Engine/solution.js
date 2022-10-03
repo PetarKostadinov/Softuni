38bf75a5c589f2658ca69ffd79df54a6e91dab43
PetarKostadinov <petarkostadinov30@gmail.com>
2022-10-03 19:05:58 +0300

    100/100

--- JavaScript/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JavaScript/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   let container = document.querySelectorAll('tbody tr');
+
+   let input = document.getElementById('searchField');
+
    function onClick() {
-      //   TODO:
+   
+      for (const row of container) {
+
+         row.classList.remove('select')
+         
+         if (row.innerHTML.includes(input.value)){
+
+            row.className = 'select'
+         };
+         
+      }
+
+      input.value = '';
 
    }
 }
