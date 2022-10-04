1c1193bd6a814078253e1201543f4e5f422b6320
PetarKostadinov <petarkostadinov30@gmail.com>
2022-10-04 10:34:27 +0300

    100/100

--- JavaScript/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JavaScript/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,27 @@
 function generateReport() {
-    //TODO
+    let checkBoxes = Array.from(document.querySelectorAll('th')).map(h => h.children[0]);
+
+    let rows = [...document.querySelectorAll('tbody tr')];
+
+    let result = [];
+
+        rows.forEach(row => {
+
+            let currRow = Array.from(row.children).reduce((obj, prop, index) => {
+
+                if(checkBoxes[index].checked){
+
+                    let headerName = checkBoxes[index].name;
+                    obj[headerName] = prop.innerText;
+                };
+
+                return obj;
+            }, {})
+
+            result.push(currRow);
+        })
+
+       
+        document.querySelector('#output').value = JSON.stringify(result);
+       
 }
