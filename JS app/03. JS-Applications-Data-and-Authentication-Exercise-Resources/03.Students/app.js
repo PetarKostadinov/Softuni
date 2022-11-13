d55fe5b90d1bb7c5f06e1a84d92a766d94a6832f
PetarKostadinov <petarkostadinov30@gmail.com>
2022-11-13 23:12:23 +0200

    100/100

--- JS app/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
+++ JS app/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
@@ -1,1 +1,68 @@
-console.log('TODO...');
+window.addEventListener('DOMContentLoaded', load)
+
+const tbody = document.getElementById('results').children[1];
+const form = document.getElementById('form');
+form.addEventListener('submit', onSubmit);
+
+function create(data) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${data.firstName}</td>
+    <td>${data.lastName}</td>
+    <td>${data.facultyNumber}</td>
+    <td>${Number(data.grade)}</td>`
+
+    return tr;
+}
+
+async function onSubmit(ev) {
+   
+    const formData = new FormData(ev.target);
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const facultyNumber = formData.get('facultyNumber');
+    const grade = formData.get('grade');
+
+    const result = await createStudent({ firstName, lastName, facultyNumber, grade });
+    tbody.appendChild(create(result))
+}
+
+async function createStudent(student) {
+
+    try {
+
+        const res = await fetch('http://localhost:3030/jsonstore/collections/students', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(student)
+        });
+
+        if (!res.ok || res.status != 200) {
+            throw new Error(data.message)
+        }
+        return res
+
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+async function load(ev) {
+
+    ev.preventDefault()
+
+    try {
+
+        const url = 'http://localhost:3030/jsonstore/collections/students';
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (!res.ok || res.status != 200) {
+            throw new Error(data.message)
+        }
+
+        Object.values(data).forEach(x => tbody.appendChild(create(x)));
+
+    } catch (error) {
+        alert(error.message)
+    }
+}
