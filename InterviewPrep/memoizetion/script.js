0b5b67767ee585ddafe1204e915b96d2a4ae4d43
PetarKostadinov <petarkostadinov30@gmail.com>
2023-04-29 20:39:20 +0300

    Memoize example

---
+++ InterviewPrep/memoizetion/script.js
@@ -0,0 +1,25 @@
+function myMemoize(fn, context) {
+    const res = {};
+    return function (...args) {
+        var argsCache = JSON.stringify(args);
+        if (!res[argsCache]) {
+            res[argsCache] = fn.call(context || this, ...args);
+        }
+        return res[argsCache];
+    };
+}
+
+const clumsyProduct = (num1, num2) => {
+    for (let i = 1; i <= 100000000; i++) { }
+    return num1 * num2;
+};
+
+const memoizedClumzyProduct = myMemoize(clumsyProduct);
+
+console.time('First call')
+console.log(memoizedClumzyProduct(9467, 7649));
+console.timeEnd('First call');
+
+console.time('Second call')
+console.log(memoizedClumzyProduct(9467, 7649));
+console.timeEnd('Second call');
