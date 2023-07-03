8125bdaea6d835bd794f922c6cccd06bf19cb967
PetarKostadinov <petarkostadinov30@gmail.com>
2023-07-03 20:38:45 +0300

    integration tests implemented

--- InterviewPrep/FT_task/FT_task/symbolMappings.js
+++
@@ -1,8 +0,0 @@
-export const symbolMappings = {
-    'EURUSD': {name: 'Euro/Dollar', link: 'https://markets.ft.com/data/equities/tearsheet/summary?s=EURUSD'},
-    'INX': {name: 'S&P 500', link: 'https://markets.ft.com/data/indices/tearsheet/summary?s=INX:IOM'},
-    'IB.1': {name: 'Brent Crude Oil', link: 'https://markets.ft.com/data/equities/tearsheet/summary?s=IB.1:IEU'},
-    'GBPUSD': {name: 'Pound/Dollar', link: 'https://markets.ft.com/data/equities/tearsheet/summary?s=GBPUSD'},
-    'FTSE': {name: 'FTSE 100', link: 'https://markets.ft.com/data/indices/tearsheet/summary?s=FTSE:FSI'},
-    'SHI': {name: 'Shanghai', link: 'https://markets.ft.com/data/indices/tearsheet/summary?s=SHI:SHH'}
-};
--- InterviewPrep/FT_task/FT_task/tests/test.js
+++ InterviewPrep/FT_task/FT_task/tests/test.js
@@ -6,51 +6,51 @@ let browser, page;
 describe('E2E tests', async function () {
     this.timeout(5000);
 
-    before(async () => {browser = await chromium.launch({headless: false, })});
+    before(async () => {browser = await chromium.launch({headless: false, slowMo: 1000})});
     after(async () => {await browser.close();});
     beforeEach(async () => {page = await browser.newPage();});
     afterEach(async () => {await page.close();});
 
-    // it('loads items from given api API', async () => {
+    it('loads items from given api API', async () => {
 
-    //     await page.goto('http://127.0.0.1:5500/index.html');
-    //     await page.waitForSelector('.markets-data__items li>a>span>span');
-    //     await page.screenshot({path: 'page.png'});
-    // })
+        await page.goto('http://127.0.0.1:5500/index.html');
+        await page.waitForSelector('.markets-data__items li>a>span');
+        await page.screenshot({path: 'page.png'});
+    })
 
-    // it('loads and shows  list items names by symbols', async () => {
+    it('loads and shows  list items names by symbols', async () => {
 
-    //     await page.goto('http://127.0.0.1:5500/index.html');
-    //     await page.waitForSelector('.markets-data__items li>a>span>span');
-    //     const content = await page.textContent('.markets-data__items');
-    //     expect(content).to.contain('Euro/Dollar');
-    //     expect(content).to.contain('S&P 500');
-    //     expect(content).to.contain('Brent Crude Oil');
-    //     expect(content).to.contain('Pound/Dollar');
-    //     expect(content).to.contain('FTSE 100');
-    // })
+        await page.goto('http://127.0.0.1:5500/index.html');
+        await page.waitForSelector('.markets-data__items li>a>span');
+        const content = await page.textContent('.markets-data__items');
+        expect(content).to.contain('Euro/Dollar');
+        expect(content).to.contain('S&P 500');
+        expect(content).to.contain('Brent Crude Oil');
+        expect(content).to.contain('Pound/Dollar');
+        expect(content).to.contain('FTSE 100');
+    })
 
-    // it('goes to Euro/Dollar details page when item is clicked', async () => {
+    it('goes to Euro/Dollar details page when item is clicked', async () => {
 
-    //     await page.goto('http://127.0.0.1:5500/index.html');
-    //     await page.waitForSelector('.markets-data__items li>a>span');
-    //     const newTabPromise = page.waitForEvent("popup");
-    //     await page.getByText('Euro/Dollar').click();
-    //     await page.waitForSelector('.mod-tearsheet-overview__header__container h1');
-    //     const visible = await page.isVisible('text=Euro/US Dollar FX Spot Rate')
-    //     expect(visible).to.be.true
-    // })
+        await page.goto('http://127.0.0.1:5500/index.html');
+        await page.waitForSelector('.markets-data__items li>a>span');
+        const newTabPromise = page.waitForEvent("popup");
+        await page.getByText('Euro/Dollar').click();
+        await page.waitForSelector('.mod-tearsheet-overview__header__container h1');
+        const visible = await page.isVisible('text=Euro/US Dollar FX Spot Rate')
+        expect(visible).to.be.true
+    })
 
-    // it('goes to Pound/Dollar details page when item is clicked', async () => {
+    it('goes to Pound/Dollar details page when item is clicked', async () => {
 
-    //     await page.goto('http://127.0.0.1:5500/index.html');
-    //     await page.waitForSelector('.markets-data__items li>a>span');
-    //     const newTabPromise = page.waitForEvent("popup");
-    //     await page.getByText('Pound/Dollar').click();
-    //     await page.waitForSelector('.mod-tearsheet-overview__header__container h1');
-    //     const visible = await page.isVisible('text=UK Pound Sterling/US Dollar FX Spot Rate')
-    //     expect(visible).to.be.true
-    // })
+        await page.goto('http://127.0.0.1:5500/index.html');
+        await page.waitForSelector('.markets-data__items li>a>span');
+        const newTabPromise = page.waitForEvent("popup");
+        await page.getByText('Pound/Dollar').click();
+        await page.waitForSelector('.mod-tearsheet-overview__header__container h1');
+        const visible = await page.isVisible('text=UK Pound Sterling/US Dollar FX Spot Rate')
+        expect(visible).to.be.true
+    })
     it('goes to S&P 500 details page when item is clicked', async () => {
 
         await page.goto('http://127.0.0.1:5500/index.html');
